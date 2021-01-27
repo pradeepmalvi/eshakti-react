bc3e2eea58f5a19ac182a9a32460d17bfad0a9c7
jitender mandal <iamjitendermandal>
2021-01-27 18:45:39 +0530

    responsiveness of category page is almost done..

--- src/components/category-page/CategoryPage.component.js
+++ src/components/category-page/CategoryPage.component.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./categoryPage.styles.scss";
 
+// local icons
 import icon1 from "../../assets/icons/01.png";
 import icon2 from "../../assets/icons/02.png";
 import icon3 from "../../assets/icons/03.png";
 import icon4 from "../../assets/icons/04.png";
+
+// react icons
+import { FiChevronDown } from "react-icons/fi";
 
 import ProductCard from "../productCard/ProductCard.component";
 import CategoryNamesFilter from "../category-names-filter/CategoryNamesFilter.component";
@@ -15,18 +19,31 @@ import ColorFilter from "../colorFilter/ColorFilter.component";
 // react icons
 import { FaDiceFour } from "react-icons/fa";
 
-export default function categoryListing() {
+export default function CategoryListing() {
+  const [showFilter, setShowFilter] = useState("");
+
+  console.log(showFilter);
+
   return (
     <div className="category-listing">
       <div className="inner-container">
-        <div className="filters-area">
+        <div className={`filters-area ${showFilter}`}>
           <CategoryNamesFilter />
           <PriceFilter />
           <SizeFilter />
           <ColorFilter />
         </div>
         <div className="product-listing">
-          <div className="details-and-viewOptions">
+          <div className="details-and-view-options">
+            <div
+              className="filter-badge"
+              onClick={() => setShowFilter(showFilter === "show" ? "" : "show")}
+            >
+              <span className="text">Filters</span>
+              <span className="icon">
+                <FiChevronDown />
+              </span>
+            </div>
             <div className="number-of-results">Showing 1-12 of 99 Results</div>
 
             <div className="right-actions">
@@ -44,6 +61,9 @@ export default function categoryListing() {
                 <img src={icon4} alt="img" />
               </div>
             </div>
+          </div>
+          <div className="number-of-results-small">
+            Showing 1-12 of 99 Results
           </div>
 
           <div className="products">
